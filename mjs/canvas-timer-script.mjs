2e093464044a8b8f4d3063e13bc30baa5fd30499
daemon-cool <damian.damian.tofik@gmail.com>
2020-01-17 09:14:14 +0100

    Deleted variable, because i won't be needed.

--- mjs/canvas-timer-script.mjs
+++ mjs/canvas-timer-script.mjs
@@ -1,5 +1,4 @@
 var canvasTimer = document.getElementById('poomodoro-canvas-timer').getContext('2d');
-var step = 0;
 var timerWidth = canvasTimer.canvas.width;
 var timerHeight = canvasTimer.canvas.height;
 var different;
