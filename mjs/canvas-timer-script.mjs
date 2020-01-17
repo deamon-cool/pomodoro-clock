c7280fb291e86a0b8350f473cc656cb3fea7d99e
daemon-cool <damian.damian.tofik@gmail.com>
2020-01-17 14:23:14 +0100

    Added script for elements canvas.

--- mjs/canvas-timer-script.mjs
+++ mjs/canvas-timer-script.mjs
@@ -1,19 +1,29 @@
-var canvasTimer = document.getElementById('poomodoro-canvas-timer').getContext('2d');
+var canvasTimer = document.getElementById('pomodoro-canvas-timer').getContext('2d');
+var canvasShadowring = document.getElementById('pomodoro-canvas-shadow').getContext('2d');
+var canvasCircle = document.getElementById('pomodoro-canvas-circle').getContext('2d');
 var startingPoint = 4.72;
 var timerWidth = canvasTimer.canvas.width;
 var timerHeight = canvasTimer.canvas.height;
 var different;
+
+canvasShadowring.arc(100, 100, 95, 0, 2 * Math.PI);
+canvasShadowring.fillStyle = 'rgba(255, 0, 0, .1)';
+canvasShadowring.fill();
+
+canvasCircle.arc(100, 100, 80, 0, 2 * Math.PI);
+canvasCircle.fillStyle = 'red';
+canvasCircle.fill();
 
 const fillCanvasTimer = (step, duration, time) => {
     different = ((step/duration) * Math.PI * 2 * 10);
 
     canvasTimer.clearRect(0, 0, timerWidth, timerHeight);
 
-    canvasTimer.lineWidth = 8;
+    canvasTimer.lineWidth = 5;
 
     canvasTimer.fillStyle = 'white';
 
-    canvasTimer.strokeStyle = 'black';
+    canvasTimer.strokeStyle = 'red';
 
     canvasTimer.textAlign = 'center';
 
@@ -22,7 +32,7 @@ const fillCanvasTimer = (step, duration, time) => {
     canvasTimer.fillText(time, 100, 110);
 
     canvasTimer.beginPath();
-    canvasTimer.arc(100, 100, 90, startingPoint, -different/10+startingPoint);
+    canvasTimer.arc(100, 100, 88, startingPoint, -different/10+startingPoint);
 
     canvasTimer.stroke();
 }
