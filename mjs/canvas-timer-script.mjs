0ed16abdd27ccf9096b2fcb92e99d16220436410
daemon-cool <damian.damian.tofik@gmail.com>
2020-01-17 09:10:08 +0100

    Added other variables to the script.

--- mjs/canvas-timer-script.mjs
+++ mjs/canvas-timer-script.mjs
@@ -1,4 +1,8 @@
 var canvasTimer = document.getElementById('poomodoro-canvas-timer').getContext('2d');
+var step = 0;
+var timerWidth = canvasTimer.canvas.width;
+var timerHeight = canvasTimer.canvas.height;
+var different;
 
 const testFunction = () => {
     console.log('message :)');
