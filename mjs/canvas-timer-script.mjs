9b0ba0e05460d02ecc62732ecd4874d6ec995173
daemon-cool <damian.damian.tofik@gmail.com>
2020-01-17 11:41:53 +0100

    Corrected name of the variable. It's better understable. Beside that added all basic functionality of function.

--- mjs/canvas-timer-script.mjs
+++ mjs/canvas-timer-script.mjs
@@ -1,10 +1,32 @@
 var canvasTimer = document.getElementById('poomodoro-canvas-timer').getContext('2d');
+var startingPoint = 4.72;
 var timerWidth = canvasTimer.canvas.width;
 var timerHeight = canvasTimer.canvas.height;
 var different;
 
 const fillCanvasTimer = (step, duration) => {
+    console.log(step, duration);
 
+    different = ((step/duration) * Math.PI * 2 * 10);
+
+    canvasTimer.clearRect(0, 0, timerWidth, timerHeight);
+
+    canvasTimer.lineWidth = 8;
+
+    canvasTimer.fillStyle = 'white';
+
+    canvasTimer.strokeStyle = 'black';
+
+    canvasTimer.textAlign = 'center';
+
+    canvasTimer.font = '25px arial';
+
+    canvasTimer.fillText(step, 100, 110);
+
+    canvasTimer.beginPath();
+    canvasTimer.arc(100, 100, 90, startingPoint, different/10+startingPoint);
+
+    canvasTimer.stroke();
 }
 
 export {fillCanvasTimer};
